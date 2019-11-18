5a67cbbdb0c83d5f0299cdefe2c3681da1c94c06
DmitryDanilov <d.danilov@vironit.co.uk>
2019-11-18 14:06:47 +0300

    +add hint for the current word +add timer

--- app/src/main/java/com/example/android/guesstheword/viewmodels/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/viewmodels/GameViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.android.guesstheword.viewmodels
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
@@ -15,7 +18,15 @@ class GameViewModel : ViewModel() {
     private var _isGameFinished = MutableLiveData<Boolean>()
     val isGameFinished: LiveData<Boolean>
         get() = _isGameFinished
+    private val _currentTime = MutableLiveData<Long>()
+    private val currentTime: LiveData<Long>
+        get() = _currentTime
+    val currentTimeString = Transformations.map(currentTime) {
+        DateUtils.formatElapsedTime(it)
+    }
     lateinit var wordList: MutableList<String>
+    lateinit var wordHint: LiveData<String>
+    private var timer: CountDownTimer? = null
 
     init {
         Timber.d("GameViewModel: created!!")
@@ -23,11 +34,13 @@ class GameViewModel : ViewModel() {
         _score.value = 0
         resetList()
         nextWord()
+        startTimer()
     }
 
     override fun onCleared() {
         super.onCleared()
         Timber.d("GameViewModel: destroyed!!")
+        timer?.cancel()
     }
 
 
@@ -60,10 +73,11 @@ class GameViewModel : ViewModel() {
 
     private fun nextWord() {
         if (wordList.isEmpty()) {
-            onGameFinished()
+            resetList()
         } else {
             //Select and remove a word from the list
             _word.value = wordList.removeAt(0)
+            showHint()
         }
     }
 
@@ -87,5 +101,33 @@ class GameViewModel : ViewModel() {
 
     fun onGameFinishComplete() {
         _isGameFinished.value = false
+    }
+
+    private fun startTimer() {
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinished()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished / ONE_SECOND
+            }
+        }.start()
+    }
+
+    private fun showHint() {
+        wordHint = Transformations.map(word) { word ->
+            val randomPosition = (1..word.length).random()
+            "Current word has " + word.length + " letters" +
+                    "\nThe letter at position " + randomPosition + " is " +
+                    word[randomPosition - 1].toUpperCase()
+        }
+    }
+
+    companion object {
+        private const val DONE = 0L
+        private const val ONE_SECOND = 1000L
+        private const val COUNTDOWN_TIME = 60000L
     }
 }
